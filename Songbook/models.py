e77f72a8de335ae3fb7dea9fade2fd028d63a888
m.makos <michal.makos@pitradwar.com>
2024-10-03 01:15:53 +0200

    Zmieniłem performers na performer

--- Songbook/models.py
+++ Songbook/models.py
@@ -1,14 +1,42 @@
 import json
-import time
 
 from Songbook.songs import songs, categories
 from Songbook.str_convert import normalize_for_search, title_to_unique_name
+
+with open('data/bands.json', encoding='utf-8') as file:
+    bands = {band["id"]: band for band in json.load(file)}
+with open('data/authors.json', encoding='utf-8') as file:
+    authors = {author["id"]: author for author in json.load(file)}
+
+
+def replace_band(song: dict):
+    band = song.get('band')
+    if not band:
+        return
+    song['band'] = bands[band]
+
+
+def replace_authors(song: dict, author: str):
+    auth = song.get(author)
+    if not auth:
+        return
+    song[author] = [authors[a] for a in auth]
+
+
+def replace(song: dict):
+    replace_authors(song, "composer")
+    replace_authors(song, "lyrics")
+    replace_authors(song, "translation")
+    replace_authors(song, "performer")
+    replace_band(song)
 
 
 def get_song(song_id: str):
     # time.sleep(2)
     with open(f'songs_manual/{song_id}.json', encoding='utf-8') as file:
-        return file.read()
+        song = json.load(file)
+        replace(song)
+        return json.dumps(song, ensure_ascii=False)
 
 
 def get_songs():
--- data/song_authors.py
+++ data/song_authors.py
@@ -1,70 +1,15 @@
 import json
 import os
-import urllib.parse
-import webbrowser
-from typing import Optional
-
-song = {}
-authors: list[dict[str, str]]
-
-with open('data/authors.json', encoding='utf-8') as file:
-    authors = json.load(file)
-
-
-wrong = 0
-
-
-def get_authors(song, a: Optional[list]) -> Optional[list]:
-    if a is None:
-        return None
-    result = []
-    for auth in a:
-        au = get_author(auth, song)
-        if au is not None:
-            result.append(au)
-
-    return result if len(result) > 0 else None
-
-
-def get_author(author: dict[str, str], song: str) -> int:
-    global wrong
-    name = author.get("name")
-    lastName = author.get("lastName")
-    if name is not None and lastName is not None:
-        for a in authors:
-            if a["name"].startswith(name) and a["lastName"] == lastName:
-                return a["id"]
-            if a["name"].startswith(name) and lastName and lastName[1:3] == ". " and lastName[3:] == a["lastName"]:
-                return a["id"]
-    if name is not None:
-        for a in authors:
-            if a.get("nickname") == name:
-                return a["id"]
-
-    print(f"Author not found: {author} for song: {song}")
-    wrong += 1
-    return 999999
-
-
-def reassign_author(song, key: str):
-    au = song.get(key)
-    a = get_authors(song["title"], au)
-    if a is not None:
-        song[key] = a
-
 
 for i, filename in enumerate(os.listdir('songs_manual')):
     if filename.endswith(".json"):
         f = os.path.join('songs_manual', filename)
         with open(f, encoding='utf-8') as file:
-            song = json.load(file)
-            reassign_author(song, "composer")
-            reassign_author(song, "lyrics")
-            reassign_author(song, "translation")
-            reassign_author(song, "performers")
-        with open(f, 'w', encoding='utf-8') as file:
-            json.dump(song, file, indent=2, ensure_ascii=False)
-
-
-print()
-print(f"Wrong authors: {wrong}")
+            song: dict = json.load(file)
+            performer = song.get('performer')
+            if performer and len(performer) == 1 and performer[0] == 100 \
+                    and 'composer' not in song and 'lyrics' not in song:
+                song['lyrics'] = performer
+                song['lyrics'] = performer
+                with open(f, 'w', encoding='utf-8') as file:
+                    json.dump(song, file, indent=2, ensure_ascii=False)
