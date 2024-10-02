3e3101e9dfffb363f457f7e76f145e417c5cf312
m.makos <michal.makos@pitradwar.com>
2024-10-03 00:55:48 +0200

    Dodałem linki do wszystkich zespołów

--- data/wikipedia_links.py
+++ data/wikipedia_links.py
@@ -1,9 +1,41 @@
 import json
-import webbrowser
+import requests
 
-with open('data/authors.json', encoding="utf-8") as file:
-    authors = json.load(file)
 
-    for author in authors:
-        if author.get('url') and "?search" in author.get('url'):
-            webbrowser.open(f"https://www.google.com/search?&q={author['name']}+{author['lastName']}")
+with open('data/bands.json', encoding='utf-8') as file:
+    bands = json.load(file)
+
+wikipedia_search_url = "https://pl.wikipedia.org/?search="
+
+
+def resolve_wikipedia_url(search_url):
+    try:
+        response = requests.get(search_url, allow_redirects=True)
+
+        if response.history:
+            return response.url
+        else:
+            return None
+    except requests.RequestException as e:
+        print(f"Error fetching URL {search_url}: {e}")
+        return None
+
+
+def resolve_band_url(band: dict) -> str | None:
+    url = wikipedia_search_url + band["name"]
+    wiki = resolve_wikipedia_url(url)
+    if "?search" in wiki:
+        wiki = resolve_wikipedia_url(url.replace("pl", "en", 1))
+
+    return wiki if "?search" not in wiki else url
+
+
+print("[")
+for band in bands:
+    wiki = resolve_band_url(band)
+    if wiki:
+        band["url"] = wiki
+
+    print(json.dumps(band, ensure_ascii=False) + ",")
+
+print("]")
